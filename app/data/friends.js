65678f2bbbf2554f6d22ff84b923af659257ff8e
William R Poulson <jessica.spildener@gmail.com>
2017-10-15 21:16:55 -0400

    validation and best match completed

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,6 +14,54 @@ var friends = [
       4,
       1
     ]
+},
+{
+  "name":"Zoey",
+  "photo":"https://i.imgur.com/ncjMYZN.jpg",
+  "scores":[
+      4,
+      5,
+      2,
+      1,
+      3,
+      2,
+      2,
+      4,
+      5,
+      5
+    ]
+},
+{
+  "name":"Henry",
+  "photo":"https://i.imgur.com/V9HTZ1g.jpg",
+  "scores":[
+      1,
+      5,
+      5,
+      5,
+      4,
+      3,
+      4,
+      5,
+      5,
+      5
+    ]
+},
+{
+  "name":"Ellie",
+  "photo":"https://i.imgur.com/Yt0F7TR.jpg",
+  "scores":[
+      4,
+      1,
+      1,
+      2,
+      3,
+      3,
+      3,
+      5,
+      4,
+      2
+    ]
 }
 
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,14 +10,29 @@ module.exports = function(app) {
 
         var userInputScores = convertScores(req.body.scores);
         var friendDifferences = getFriendDifferences(userInputScores, friends);
+        var bestMatchIndex = getIndexOfBestMatch(friendDifferences);
+        var bestMatch = friends[bestMatchIndex];
+        console.log(bestMatch);
+        res.json(bestMatch);
     });
 }
 
 function convertScores(scores) {
+    var intScores = [];
     for (var i = 0; i < scores.length; i++) {
-        scores[i] = parseInt(scores[i]);
+        var score = parseInt(scores[i]);
+        intScores.push(score);
     }
-    return scores;
+    return intScores;
+}
+
+function getFriendDifferences(userInputScores, friends) {
+    var friendDifferences = [];
+    for (var i = 0; i < friends.length; i++) {
+        var totalDifference = getTotalDifference(userInputScores, friends[i].scores);
+        friendDifferences.push(totalDifference);
+    }
+    return friendDifferences;
 }
 
 function getTotalDifference(newFriendScores, oldFriendScores) {
@@ -28,11 +43,14 @@ function getTotalDifference(newFriendScores, oldFriendScores) {
     return totalDifference;
 }
 
-function getFriendDifferences(userInputScores, friends) {
-    var friendDifferences = [];
-    for (var i = 0; i < friends.length; i++) {
-        var totalDifference = getTotalDifference(userInputScores, friends[i].scores);
-        friendDifferences.push(totalDifference);
+function getIndexOfBestMatch(friendDifferences) {
+    var index = 0;
+    var value = friendDifferences[0];
+    for (var i = 1; i < friendDifferences.length; i++) {
+        if (friendDifferences[i] < value) {
+            value = friendDifferences[i];
+            index = i;
+        }
     }
-    return friendDifferences;
+    return index;
 }
